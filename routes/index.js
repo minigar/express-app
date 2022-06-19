be8b5b108ef4891db686836480d010bfedd94daf
minigar <dd8556205@gmail.com>
2022-06-19 15:21:51 +0300

    add 2 files to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const posts = require('./posts');
 const users = require('./users');
 const books = require('./books');
 const refreshTokens = require('./refresh-tokens');
+const postsComments = require('./post-comments');
+const booksComments = require('./books-comments');
 
 
 const router = express.Router();
 
-router.use(books, posts, users, refreshTokens);
+router.use(books, posts, users, refreshTokens, postsComments, booksComments);
 
 module.exports = router
