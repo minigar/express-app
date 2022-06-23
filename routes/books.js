90d0e560de7eebfccf62ed125ef6d159a4fb1acd
minigar <dd8556205@gmail.com>
2022-06-23 16:14:23 +0300

    add controllers to routes

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const passport = require('passport');
+require('../lib/passport-config')(passport)
+
+const { getAllBooks, createNewBook, updateBook, deleteBook } = require('../controllers/books')
 
 const router = express.Router();
+
+
+router.get('/books', getAllBooks)
+router.post('/books', passport.authenticate('jwt', { session: false}), createNewBook)
+router.put('/books', passport.authenticate('jwt', { session: false}), updateBook)
+router.delete('/books', passport.authenticate('jwt', { session: false}), deleteBook)
 
 
 module.exports = router
