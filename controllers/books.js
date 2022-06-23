b12b2aa78cd8f6a6ad61601f15b89998a2993246
minigar <dd8556205@gmail.com>
2022-06-23 17:37:45 +0300

    add put && delete books controllers

--- controllers/books.js
+++ controllers/books.js
@@ -2,11 +2,20 @@ const Book = require('../models/Book');
 const User = require('../models/User');
 
 const getOneBookById = async(req, res) => {
-    
+    const bookId = req.params._id;
+    const book = await Book.findById(bookId);
+
+    if (!book) {
+        res.status(404).json("Book not found!");
+    }
+
+    else{
+        res.status(200).json(book);
+    }
 }
 
 const getAllBooks = async(req, res) => {
-    const books = Book.find();
+    const books = await Book.find();
 
     res.status(200).json(books)
 }
@@ -35,11 +44,52 @@ const createNewBook = async(req, res) => {
 }   
 
 const updateBook = async(req, res) => {
+    const bookId = req.params._id;
+    const book = await Book.findById(bookId);
+    const userId = req.user._id
+    const compareUserId = book.user._id.toString() == userId.toString()
 
+    if (!book) {
+        res.status(404).json("Book not found!");
+    }
+
+    else{
+        if (compareUserId) {
+            const { name, description, cost } = req.body;
+            const filter = { _id: bookId };
+            const update = { name: name, description: description, cost: cost };
+
+            const updatedBook = await Book.findOneAndUpdate(filter, update, { new: true });
+            res.status(200).json(updatedBook)
+        }
+
+        else{
+            res.status(400).json("It is not your book you can't change it!")
+        }
+    }
 }
 
 const deleteBook = async(req, res) => {
+    const bookId = req.params._id;
+    const book = await Book.findById(bookId);
+    const userId = req.user._id;
+    const compareUserId = book.user._id.toString() == userId.toString()
 
+    if (!book) {
+        res.status(404).json("Book not found!");
+    }
+
+    else{
+        if (compareUserId) {
+            const conditions = { _id: bookId }
+            await Book.findOneAndDelete(conditions)
+            res.status(200).json("You book has been deleted!")
+        }
+
+        else{
+            res.status(400).json("It is not your book you can't delete it!")
+        }
+    }
 }
 
 module.exports = {
