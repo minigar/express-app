ec761fe57c7279583123abcb03d3b5d75eaaabc1
minigar <dd8556205@gmail.com>
2022-06-23 16:15:12 +0300

    -a

--- routes/books.js
+++ routes/books.js
@@ -2,12 +2,13 @@ const express = require('express');
 const passport = require('passport');
 require('../lib/passport-config')(passport)
 
-const { getAllBooks, createNewBook, updateBook, deleteBook } = require('../controllers/books')
+const { getAllBooks, createNewBook, updateBook, deleteBook, getOneBookById } = require('../controllers/books')
 
 const router = express.Router();
 
 
 router.get('/books', getAllBooks)
+router.get('/books/:_id', getOneBookById)
 router.post('/books', passport.authenticate('jwt', { session: false}), createNewBook)
 router.put('/books', passport.authenticate('jwt', { session: false}), updateBook)
 router.delete('/books', passport.authenticate('jwt', { session: false}), deleteBook)
