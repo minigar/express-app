7dec2846c36624a6a9adff38d14088fe80248b9f
minigar <dd8556205@gmail.com>
2022-06-25 00:04:50 +0300

    add createComment controller

---
+++ controllers/books-comments.js
@@ -0,0 +1,43 @@
+const Book = require('../models/Book')
+
+const getCommentsfromOnePost = async(req, res) => {
+    const bookId = req.params.bookId;
+    const book = await Book.findOne({ _id: bookId });
+    if (!book) {
+        res.status(404).json("Book not found")
+    }
+
+    const comments = book.comments;
+
+    res.status(200).json(comments);
+}
+
+const createComment = async(req, res) => {
+    const bookId = req.params.bookId;
+    const book = await Book.findById(bookId);
+
+    if (!book) {
+        res.status(404).json("Book not found!");
+    }
+
+    const { body } = req.body;
+    const user = req.user;
+
+    await book.comments.unshift({ body, user });
+    res.status(200).json(await book.save());
+}
+
+const updateComment = async(req, res) => {
+    
+}
+
+const deleteComment = async(req, res) => {
+
+}
+
+module.exports = {
+    getCommentsfromOnePost,
+    createComment,
+    updateComment,
+    deleteComment
+}
