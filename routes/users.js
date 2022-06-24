3517f5b2f085219591a1d88b61a6022189c3ae3d
minigar <dd8556205@gmail.com>
2022-06-24 22:06:17 +0300

    add PUT request to /users/:_id/about

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const usersController = require('../controllers/users');
-
+const passport = require('passport');
+require('../lib/passport-config')(passport);
+const { getUserById, getUsers, aboutUser, updateAboutMe, changePassword, changeName } = require('../controllers/users');
 
 const router = express.Router();
+
+router.get('/users', getUsers);
+router.get('/users/:_id', getUserById);
+router.get('/users/:_id/about', aboutUser);
+router.put('/users/:_id/about', passport.authenticate('jwt', { session: false}), updateAboutMe);
+router.put('/users/:_id/password/change', passport.authenticate('jwt', { session: false}), changePassword);
+router.put('/users/:_id/name/change', passport.authenticate('jwt', { session: false}), changeName)
 
 module.exports = router
