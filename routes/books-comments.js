d340beb9c26d0ed1f06c5bd27af1a095e136009b
minigar <dd8556205@gmail.com>
2022-06-25 00:03:41 +0300

    add GET POST PUT DELETE routes to /book/:bookId/comment

--- routes/books-comments.js
+++ routes/books-comments.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const passport = require('passport');
+require('../lib/passport-config')(passport)
+
+const { getCommentsfromOnePost, createComment, deleteComment, updateComment } = require('../controllers/books-comments');
 
 const router = express.Router();
+
+router.get('/books/:bookId/comments', getCommentsfromOnePost);
+router.post('/books/:bookId/comments', passport.authenticate('jwt', { session: false}), createComment);
+router.post('/books/:bookId/comments', passport.authenticate('jwt', { session: false}), deleteComment);
+router.post('/books/:bookId/comments', passport.authenticate('jwt', { session: false}), updateComment)
 
 
 module.exports = router
