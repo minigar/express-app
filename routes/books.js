bfa3218c233e1d267fcadc224773eb3097edd4db
minigar <dd8556205@gmail.com>
2022-06-26 03:15:38 +0300

    add billet for veiws

--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const Book = require('../models/Book');
 const passport = require('passport');
-require('../lib/passport-config')(passport)
+require('../lib/passport-config')(passport);
 
 const { getAllBooks, createNewBook, updateBook, deleteBook, getOneBookById } = require('../controllers/books')
 
 const router = express.Router();
 
+// router.get('/bookss', passport.authenticate('jwt', { session: false}), async(req, res) => {
+//     const books = await Book.find();
+//     res.render('books.ejs', { title: 'books', books: books})
+// })
 
 router.get('/books', getAllBooks)
 router.get('/books/:_id', getOneBookById)
