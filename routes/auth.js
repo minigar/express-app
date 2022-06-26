616f78de9e47244e549598556053316809e92db4
minigar <dd8556205@gmail.com>
2022-06-26 03:15:24 +0300

    add billet for veiws

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,15 @@ const router = express.Router();
 const { getNewUser, logIn } = require('../controllers/auth')
 
 
+// router.get('/register', async(req, res) => {
+//     res.render('register.ejs', { title: 'Register' })
+// });
+
 router.post('/register', getNewUser);
+
+// router.get('/login', async(req, res) => {
+//     res.render('login.ejs', { title: 'Log In' })
+// })
 
 router.post('/login', logIn);
 
