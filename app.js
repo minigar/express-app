b43a96d7a5c7ca3851cc88ce71176277cba2a922
minigar <dd8556205@gmail.com>
2022-06-26 03:18:13 +0300

    -a

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const passport = require('passport');
 require('./lib/passport-config')(passport)
 const session = require('express-session')
+const cookieParser = require('cookie-parser');
 
 const config = require('./lib/config');
 const mongooseConfig = require('./lib/mongoose-config');
@@ -16,6 +17,14 @@ const app = express();
 // connect to mongoDB
 mongooseConfig();
 
+app.use(session({ // session middleware
+    secret: config.jwt.secret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: config.session.cookie
+}));
+
+
 // add middleware for handlers
 handlers.forEach((h) => {app.use(h)});
 
@@ -26,22 +35,14 @@ app.use(express.static('views'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
+app.use(cookieParser());
+
 // add prefix to routes(without auth.js)
 app.use('/api', routes)
 app.use('/api/auth', auth)
-
-
-app.use(session({ // session middleware
-    secret: config.jwt.secret,
-    resave: false,
-    saveUninitialized: false,
-    cookie: config.session.cookie
-}));
-
 
 // passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-app.listen(config.port, () => console.log(`Server has been started at ${config.port} port`))
+app.listen(config.port, () => console.log(`Server has been started at ${config.port} port`));
