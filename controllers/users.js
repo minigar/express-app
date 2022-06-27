16348ce360751c656be844aebe23e0cc2dc5f46b
minigar <dd8556205@gmail.com>
2022-06-27 17:09:27 +0300

    add changePassword controller for user

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require('bcryptjs');
 
 const getUserById = async(req, res) => {
     const userId = req.params._id;
@@ -45,11 +46,41 @@ const updateAboutMe = async(req, res) => {
 }
 
 const aboutUser = async(req, res) => {
+    const userId = req.params._id;
+    const user = await User.findById(userId);
+    if (!user) {
+        res.status(404).json("User not found!");
+    }
 
+    const aboutUser = user.aboutMe;
+
+    res.status(200).json(aboutUser);
 }
 
 const changePassword = async(req, res) => {
+    const { email, password, newPassword } = req.body;
+    const user = await User.findById( req.user._id );
+    
+    if ( email !== user.email ) {
+        res.status(400).json("Incorrect email");
+    }
 
+    if (password.length < 7) {
+        res.status(400).json('Password must have 8 or more characters');
+    }
+
+    const comparePassword = await bcrypt.compare(password, user.password);
+
+    if (comparePassword) {
+        const salt = await bcrypt.genSalt(10);  
+        const hash = await bcrypt.hash(newPassword, salt);
+
+        const filter = { _id: req.user._id };
+        const update = { password: hash };
+
+        const updatedUser = await User.findOneAndUpdate(filter, update, { new: true });
+        res.status(200).json(updatedUser);
+    }
 }
 
 const changeName = async(req, res) => {
