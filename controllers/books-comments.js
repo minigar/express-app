05b58661d48baa15aed7459aa87999d200cc4db7
minigar <dd8556205@gmail.com>
2022-07-02 02:55:23 +0300

    add deleteBookComment controller

--- controllers/books-comments.js
+++ controllers/books-comments.js
@@ -31,11 +31,32 @@ const updateComment = async(req, res) => {
     const bookId = req.params.bookId;
     const book = await Book.findById(bookId);
     const user = req.user;
-    
 }
 
 const deleteComment = async(req, res) => {
+    const { bookId, commentId} = req.params;
+    const book = await Book.findById(bookId);
+    const currentUserId = req.user._id;
+    const compareUserId = book.user._id.toString() == currentUserId.toString()
 
+    if (!book) {
+        res.status(404).json("Book not found!");
+    }
+
+    if (!compareUserId) {
+        res.status(400).json("It is not your book you can't delete it!'")
+    }
+
+    const commentIndex = book.comments.findIndex(comment => comment._id.toString() === commentId)
+
+    console.log(commentIndex)
+
+    if (commentIndex < 0) {
+        res.status(404).json("Comment not found")
+    }
+
+    book.comments.splice(commentIndex, 1);
+    res.status(200).json(await book.save());
 }
 
 module.exports = {
