dba5d310c7b4b93befd4fc6798cb45e0c82954a3
minigar <dd8556205@gmail.com>
2022-07-02 02:37:16 +0300

    update addLikeToBookComment controller and add deleteLikeFromBookComment controller

--- controllers/books-comments-likes.js
+++ controllers/books-comments-likes.js
@@ -5,27 +5,58 @@ const addLikeToBookComment = async(req, res) => {
     const { bookId, commentId } = req.params;
     const book  = await Book.findById(bookId);
     const currentUserId = req.user._id;
-    const commentIndex = book.comments;
+
+    if (!book) {
+        res.status(404).json("Book not found!");
+    }
+    const commentIndex = book.comments.findIndex(comment => comment._id.toString() === commentId) // find comment index
+
+    if (commentIndex < 0) {
+        res.status(404).json("Comment not found!")
+    }
+
+    const commentLikeIndex = book.comments[commentIndex].likes.find(l => l.user._id.toString() === currentUserId.toString()) // find like index
+
+    if (commentLikeIndex) {
+        res.status(400).json("You alredy liked this comment!");
+    }
+
+    else{
+        book.comments[commentIndex].likes.unshift({ user: currentUserId }); // cuz in model we have user like Schema.Types.ObjectId
+        res.status(200).json(await book.save());
+    }
+}
+
+const deleteLikeFromBookComment = async(req, res) => {
+    const { bookId, commentId, likeId} = req.params;
+    const book = await Book.findById(bookId);
+    const currentUserId = req.user._id;
+    const compareUserId = book.user._id.toString() == currentUserId.toString()
 
     if (!book) {
         res.status(404).json("Book not found!");
     }
 
-    commentIndex.findIndex(async(comment) => {
-        comment._id.toString() === commentId;
-    
-        if (comment.likes.find((l) => l.user._id.toString() === currentUserId.toString())) {
-            res.status(400).json("You alredy liked this comment!");
-        }
+    if (!compareUserId) {
+        res.status(400).json("It is not your book you can't delete it!'")
+    }
 
-        else{
-            comment.likes.unshift({ user: currentUserId });
-            res.status(200).json(await book.save());
-        }
-    })
+    const commentIndex = book.comments.findIndex(comment => comment._id.toString() === commentId)
+
+    if (commentIndex < 0) {
+        res.status(404).json("Comment not found")
+    }
+
+    const commentLikeIndex = await book.comments[commentIndex].likes.findIndex(l => l._id.toString() === likeId.toString());
+
+    if (commentLikeIndex < 0)  {
+        res.status(404).json("Like not found!");
+    }
+
+    book.comments[commentIndex].likes.splice(commentLikeIndex, 1);
+    res.status(200).json(await book.save());
 }
 
-const deleteLikeFromBookComment = async(req, res) => {}
 
 
 module.exports = {
