b99c331205f5a02905a0fb7f2826952b79046cee
minigar <dd8556205@gmail.com>
2022-07-01 18:22:46 +0300

    --no-edit

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,11 @@ const books = require('./books');
 const refreshTokens = require('./refresh-tokens');
 const postsComments = require('./post-comments');
 const booksComments = require('./books-comments');
+const booksCommentsLikes = require('./books-comments-likes');
 const bookLikes = require('./books-likes');
 
 const router = express.Router();
 
-router.use(books, posts, users, refreshTokens, postsComments, booksComments, bookLikes);
+router.use(books, posts, users, refreshTokens, postsComments, booksComments, bookLikes, booksCommentsLikes);
 
 module.exports = router
